66553f90a22b64b88f372c1c057a822a59280bca
Yousef <yousefm2315@gmail.com>
2025-05-02 17:43:14 -0400

    e

--- run.py
+++ run.py
@@ -26,16 +26,7 @@ CORS(app,
 
 #database connection details
 DATABASE_URL = os.environ.get('DATABASE_URL')
-
-if DATABASE_URL:
-    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-else:
-    DB_USER = os.environ.get('DB_USER')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD')
-    DB_HOST = os.environ.get('DB_HOST')
-    DB_NAME = os.environ.get('DB_NAME')
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #connects sqlalchemy to flask app
