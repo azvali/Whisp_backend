5aa129951c24e92bdb10312c6f50332994143add
Yousef <yousefm2315@gmail.com>
2025-05-07 02:28:54 -0400

    e

--- run.py
+++ run.py
@@ -300,5 +300,5 @@ if __name__ == '__main__':
         db.create_all()
     port = int(os.environ.get("PORT", 5000))
     
-    # app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
     socketio.run(app, host="0.0.0.0", port=port, debug=True)
